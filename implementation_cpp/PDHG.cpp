f43cf2d11c298c55fb1a19ea2e3cba0dc823566f
Try2bebtr <whtu23@m.fudan.edu.cn>
2023-09-19 17:00:54 +0800

    use Gurobi to read mps file, use visual studio

--- implementation_cpp/PDHG.cpp
+++ implementation_cpp/PDHG.cpp
@@ -1,4 +1,6 @@
-#include <vector>
+// #include <vector>
+#include "gurobi_c++.h"
+#include <iostream>
 
 const int M = 100, N = 100;
 
@@ -11,7 +13,7 @@ struct Iterates
 struct Params
 {
     double eta{1e-2}, beta{1e-1};
-    int max_iter{1e5};
+    int max_iter{10^5};
 };
 
 void PrimalDualStep(Iterates &z, Params p)
@@ -22,7 +24,7 @@ void update(Iterates &z)
 {
     for (int i = 0; i < M + N; i++)
     {
-        z.z_bar[i] = z.t / (z.t + 1) * z.z_hat[i] + 1 / (z.t + 1) * z.z_hat[i];
+        z.z_bar[i] = z.t*1.0 / (z.t + 1) * z.z_hat[i] + 1.0 / (z.t + 1) * z.z_hat[i];
     }
 }
 
@@ -32,6 +34,8 @@ void PrimalDualMethods()
     Iterates z;
     Params p;
 
+    GRBEnv env = GRBEnv();
+
     while (true)
     {
         while (true)
@@ -39,4 +43,37 @@ void PrimalDualMethods()
             PrimalDualStep(z, p);
         }
     }
+}
+
+void load_model()
+{
+    GRBEnv env = GRBEnv();
+    GRBModel model = GRBModel(env, "data/qap10.mps");
+
+    // Get the number of variables in the model.
+    int numVars = model.get(GRB_IntAttr_NumVars);
+
+    // Get the number of constraints in the model.
+    int numConstraints = model.get(GRB_IntAttr_NumConstrs);
+
+    // Get the coefficients from the model.
+    double c[] = { model.get(GRB_DoubleAttr_Obj,model.getVars()) };
+
+    // Create a two-dimensional pointer to an array of double values to store the constraint matrix.
+    double** A = new double* [numConstraints];
+    for (int i = 0; i < numConstraints; i++) {
+        A[i] = new double[numVars];
+    }
+
+    // Get the constraint matrix from the model.
+    //model.get(GRB_DoubleAttr_A, A);
+
+    // Get the right-hand side vector from the model.
+    double b[] = { model.get(GRB_DoubleAttr_RHS) };
+}
+
+int main()
+{
+    load_model();
+    return 0;
 }
