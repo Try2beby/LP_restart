7fea69f67a12bf4ab42296912c7f9a24b19763dd
Try2bebtr <whtu23@m.fudan.edu.cn>
2024-05-08 12:30:04 +0000

    Update gurobi solve file

--- implementation_cpp/jupyternb/solveByGurobi.py
+++ implementation_cpp/jupyternb/solveByGurobi.py
@@ -3,13 +3,11 @@ import os
 
 # curDir = os.path.dirname(os.path.realpath(__file__))
 
-presolvedDir = "/home/twh/data_manage/cache/presolved"
+dataDir = "/home/twh/work/LP_tests/data/gendata"
 logDir = "/home/twh/work/PDLP/implementation_cpp/log/gurobi"
 # find all files in presolvedDir end with .mps and not start with pgrk_
 mpsFiles = [
-    f
-    for f in os.listdir(presolvedDir)
-    if f.endswith(".mps") and not f.startswith("pgrk_")
+    f for f in os.listdir(dataDir) if f.endswith(".mps") and f.startswith("trans")
 ]
 
 # # find all files in presolvedDir end with .mps and start with pgrk_
@@ -38,7 +36,12 @@ print("Solve " + str(len(mpsFiles)) + " mps files by gurobi.")
 print(mpsFiles)
 
 for mps in mpsFiles:
-    m = gp.read(os.path.join(presolvedDir, mps))
+    # if mps already in logDir, continue
+    if mps.replace(".mps", ".log") in os.listdir(logDir):
+        print(f"Problem {mps} has been solved.")
+        continue
+
+    m = gp.read(os.path.join(dataDir, mps))
     # set barrier algorithm
     m.Params.method = 2
     # set time limit
@@ -46,7 +49,9 @@ for mps in mpsFiles:
     # set log file
     m.Params.LogFile = os.path.join(logDir, mps.replace(".mps", ".log"))
     # disable crossover
-    m.setParam("Crossover", 0)
+    m.Params.Crossover = 1
+    m.Params.Threads = 1
+
     m.optimize()
     # m.write(os.path.join(logDir, mps.replace(".mps", ".sol")))
     print(
